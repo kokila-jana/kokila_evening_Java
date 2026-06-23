8aa590d7963f575c4744b51c946af09b64411dd2
kokila-jana <kokilagur2016@gmail.com>
2026-06-23 19:01:37 +0530

    Create ArrayConcepts.java

---
+++ ArrayConcepts.java
@@ -0,0 +1,228 @@
+package afternoon;
+import java.util.Arrays;
+import java.util.Scanner;
+public class ArrayConcepts
+{
+	public static void main(String[] args)
+	{
+//		int a=10;
+//		a=40;
+//		a=6556;
+//		System.out.println(a);
+		
+//		//c c++ Array
+//		int a[]= {13,6,59,1,98,41,6};
+//		System.out.println(a.length);
+//		/*
+//		 *   13  6  59  1  98  41  6
+//		 *   0   1   2  3  4    5  6
+//		 */
+////		System.out.println(a[3]);
+//		
+//		for(int i=0;i<a.length;i++)
+//		{
+//			System.out.println(a[i]);
+//		}
+		
+		/*
+		 * Java Array Syntax
+		 * data-type variable[]=new data-type[size];
+		 */
+		
+//		int  a[]=new int[5];
+//		a[0]=78;
+//		a[1]=74;
+//		a[2]=71;
+//		a[3]=72;
+//		a[4]=76;
+//		for(int i=0;i<a.length;i++)
+//		{
+//			System.out.println(a[i]);
+//		}
+		
+//		Scanner sc=new Scanner(System.in);
+//		System.out.println("enter array size...");
+//		int size=sc.nextInt();
+//		int a[]=new int[size];
+//		System.out.println("Enter array elements");
+//		for(int i=0;i<size;i++)
+//		{
+//			a[i]=sc.nextInt();
+//		}
+//		System.out.println("Array elements....");
+//		for(int i=0;i<a.length;i++)
+//		{
+//			System.out.println(a[i]);
+//		}
+//		System.out.println("Enter your key..");
+//		int key=sc.nextInt();
+//		for(int i=0;i<a.length;i++)
+//		{
+//			if(a[i]==key)
+//			{
+//				System.out.println("key found "+i+" index position");
+//			}
+//		}
+		
+		
+		
+//		System.out.println("reversed Array elements....");
+//		for(int i=a.length-1;i>=0;i--)
+//		{
+//			System.out.println(a[i]);
+//		}
+		
+		
+		
+		
+//		Arrays.sort(a);
+//		for(int i=0;i<a.length;i++)
+//		{
+//			for(int j=i+1;j<a.length;j++)
+//			{
+//				if(a[i]>a[j])
+//				{
+//					int temp=a[i];
+//					a[i]=a[j];
+//					a[j]=temp;
+//				}
+//			}
+//		}
+//		
+//		
+//		
+//		
+//		
+//		System.out.println("Ascending Array elements....");
+//		for(int i=0;i<a.length;i++)
+//		{
+//			System.out.println(a[i]);
+//		}
+		
+		
+		
+		
+		
+//		int max=a[0];
+//		for(int i=1;i<a.length;i++)
+//		{
+//			if(max<a[i])
+//			{
+//				max=a[i];
+//			}
+//		}
+//		System.out.println("max element is..."+max);
+//		
+//		int min=a[0];
+//		for(int i=1;i<a.length;i++)
+//		{
+//			if(min>a[i])
+//			{
+//				min=a[i];
+//			}
+//		}
+//		System.out.println("min element is..."+min);
+//		
+		/*     0 1 2
+		 * 0-->1,2,3
+		 * 1-->4,5,6
+		 * 2-->7,8,9
+		 */
+		
+//		int a[][]= {{1,2,3},{4,5,6},{7,8,9}};
+//		for(int i=0;i<3;i++)
+//		{
+//			for(int j=0;j<3;j++)
+//			{
+//				System.out.print(a[i][j]+" ");
+//			}
+//			System.out.println();
+//		}
+		
+		/*
+		 * Java syntax
+		 * 
+		 * data-type variable[][]=new data-type[row][column];
+		 */
+		
+//		int a[][]=new int[2][3];
+//		a[0][0]=12;
+//		a[0][1]=74;
+//		a[0][2]=45;
+//		a[1][0]=96;
+//		a[1][1]=32;
+//		a[1][2]=85;
+//		for(int i=0;i<2;i++)
+//		{
+//			for(int j=0;j<3;j++)
+//			{
+//				System.out.print(a[i][j]+" ");
+//			}
+//			System.out.println();
+//		}
+		
+//		Scanner sc=new Scanner(System.in);
+//		System.out.println("Enter row and column size...");
+//		int row=sc.nextInt();
+//		int col=sc.nextInt();
+//		int a[][]=new int[row][col];
+//		int b[][]=new int[row][col];
+//		System.out.println("Enter first array elements...");
+//		for(int i=0;i<row;i++)
+//		{
+//			for(int j=0;j<col;j++)
+//			{
+//				a[i][j]=sc.nextInt();
+//			}
+//		}
+//		System.out.println("Enter second array elements...");
+//		for(int i=0;i<row;i++)
+//		{
+//			for(int j=0;j<col;j++)
+//			{
+//				b[i][j]=sc.nextInt();
+//			}
+//		}
+//		System.out.println("first Array elements are...");
+//		for(int i=0;i<row;i++)
+//		{
+//			for(int j=0;j<col;j++)
+//			{
+//				System.out.print(a[i][j]+"  ");
+//			}
+//			System.out.println();
+//		}
+//		System.out.println("second Array elements are...");
+//		for(int i=0;i<row;i++)
+//		{
+//			for(int j=0;j<col;j++)
+//			{
+//				System.out.print(b[i][j]+"  ");
+//			}
+//			System.out.println();
+//		}
+//		
+//		int c[][]=new int[row][col];
+//		for(int i=0;i<row;i++)
+//		{
+//			for(int j=0;j<col;j++)
+//			{
+//				c[i][j]=a[i][j]+b[i][j];
+//			}
+//		}
+//		
+//		System.out.println("The result array is...");
+//		for(int i=0;i<row;i++)
+//		{
+//			for(int j=0;j<col;j++)
+//			{
+//				System.out.print(c[i][j]+"  ");
+//			}
+//			System.out.println();
+//		}
+		
+		
+		
+	}
+
+}
