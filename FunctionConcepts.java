472815069aa641db05b80dc7fadc3c6ac0447c46
kokila-jana <kokilagur2016@gmail.com>
2026-06-23 19:01:08 +0530

    Create FunctionConcepts.java

---
+++ FunctionConcepts.java
@@ -0,0 +1,139 @@
+package afternoon;
+
+import java.util.Scanner;
+
+//public class FunctionConcepts
+//{
+//	static int add(int a,int b)
+//	{
+//		return a+b;
+//	}
+//
+//	public static void main(String[] args)
+//	{
+//		int a=add(45,63);
+//		System.out.println(a);
+//		System.out.println(add(74,12));
+//	}
+//
+//}
+/*
+ * public static return-type functionName(arguments/parameters)
+ * {
+ * 		//block
+ * }
+ */
+
+//public class FunctionConcepts
+//{
+//	static int linearSearch()
+//	{
+//		Scanner sc=new Scanner(System.in);
+//		int a[]= {1,8,2,6,6,18,5,65};
+//		System.out.println("Enter your key");
+//		int key=sc.nextInt();
+//		for(int i=0;i<a.length;i++)
+//		{
+//			if(a[i]==key)
+//			{
+//				return i;
+//			}
+//		}
+//		return -1;
+//	}
+//	public static void main(String[] args)
+//	{
+//		int a=linearSearch();
+//		if(a==-1)
+//		{
+//			System.out.println("element not found...");
+//		}
+//		else
+//		{
+//			System.out.println("Element found in "+a+" index position");
+//		}
+//	}
+//
+//}
+
+//public class FunctionConcepts
+//{
+//	static void squareDigit(int a)
+//	{
+//		while(a>0)
+//		{
+//			int c=a%10;
+//			System.out.println(c*c);
+//			a=a/10;
+//		}
+//	}
+//		
+//	public static void main(String[] args)
+//	{
+//		squareDigit(1234);
+//		squareDigit(123465);
+//	}
+//
+//}
+
+//public class FunctionConcepts
+//{
+//	static void incrment()
+//	{
+//		int a=10;
+//		a+=5;
+//		System.out.println(a);
+//	}
+//	public static void main(String[] args)
+//	{
+//		incrment();
+//		incrment();
+//	}
+//
+//}
+
+
+//public class FunctionConcepts
+//{
+//	static int fact(int a)//5
+//	{
+//		if(a==0 || a==1)
+//		{
+//			return 1;
+//		}
+//		else
+//		{
+//			return a*fact(a-1);
+//		}
+//	}
+//	
+//	public static void main(String[] args)
+//	{
+//		System.out.println(fact(5));
+//	}
+//
+//}
+
+//public class FunctionConcepts
+//{
+//	static int b=10;
+//	static void add()
+//	{
+//		b+=5;
+//		System.out.println(b);
+//		
+//	}
+//	
+//	public static void main(String[] args)
+//	{
+//		add();
+//		add();
+//		add();
+//	}
+/*
+ * 3 types of variable
+ * 1.local variable
+ * 2.global variable
+ * 3.static variable
+ */
+//}
